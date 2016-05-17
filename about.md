e4c53200a16b45e79651ac69b735c1734f0a69c3
Michael Thiesen <micthiesen@gmail.com>
2016-05-16 18:24:17 -0700

    start fresh

--- about.md
+++
@@ -1,8 +0,0 @@
----
-layout: default
-title: About Me
----
-
-Hello, my name is Michael. I'm a software developer, currently working with the team at [Eventsage](http://www.eventsage.com).
-
-This site is a work in progress, expect to see more here. Feel free to [contact me](mailto:micthiesen@gmail.com) if you want. I'm friendly, I think.
--- gruntfile.js
+++ gruntfile.js
@@ -1,97 +1,20 @@
+'use strict';
+
 module.exports = function(grunt) {
   grunt.initConfig({
-    clean: {
-      dist: ['build/*']
-    },
-    copy: {
-      bootstrap: {
-        cwd: 'bower_components/bootstrap/dist/',
-        expand: true,
-        src: ['fonts/*', 'js/bootstrap.min.js'],
-        dest: 'build/'
-      },
-      fontAwesome: {
-        cwd: 'bower_components/components-font-awesome/',
-        expand: true,
-        src: ['fonts/*'],
-        dest: 'build/'
-      },
-      jquery: {
-        cwd: 'bower_components/jquery/dist/',
-        expand: true,
-        src: ['jquery.min.js'],
-        dest: 'build/js/'
-      },
-      moment: {
-        cwd: 'bower_components/moment/min/',
-        expand: true,
-        src: ['moment.min.js'],
-        dest: 'build/js/'
-      }
-    },
-    less: {
-      prod: {
-        options: {
-          compress: true,
-          cleancss: true
-        },
-        files: {
-          'build/style.css': 'style.less'
-        }
-      }
-    },
-    jekyll: {
-      options: {
-        drafts: true,
-        future: true
-      },
-      build: {}
-    },
     connect: {
       server: {
         options: {
-          base: ['_site'],
+          base: ['.'],
           hostname: 'localhost',
-          port: 8000
+          port: 8000,
+          keepalive: true
         }
-      }
-    },
-    watch: {
-      less: {
-        files: ['style.less'],
-        tasks: ['less:prod', 'jekyll']
-      },
-      jekyll: {
-        files: [
-          '*.html',
-          '*.js',
-          '*.md',
-          '_config.yml',
-          '_drafts/*.*',
-          '_includes/*.*',
-          '_layouts/*.*',
-          '_posts/*.*',
-          '_data/*.*',
-          'images/*.*',
-        ],
-        tasks: ['jekyll']
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-jekyll');
 
-  grunt.registerTask(
-    'default',
-    ['clean:dist', 'copy', 'less:prod', 'jekyll', 'connect', 'watch']
-  );
-  grunt.registerTask(
-    'build',
-    ['clean:dist', 'copy', 'less:prod']
-  );
+  grunt.registerTask('default', ['connect']);
 };
